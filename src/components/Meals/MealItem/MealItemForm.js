3fe2a6f28f0cb5a4489db191ac84db833f1d7f18
juancarlosbs <juanrov_@hormail.com>
2021-04-22 11:13:43 -0300

    style: static MealsForm

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,18 +1,23 @@
-import classes from './MealItem.module.css';
+import Input from '../../UI/Input';
+import classes from './MealItemForm.module.css';
 
-const MealItem = (props) => {
-  const price = `$${props.price.toFixed(2)}`;
-
+const MealItemForm = (props) => {
   return (
-    <li className={classes.meal}>
-      <div>
-        <h3>{props.name}</h3>
-        <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
-      </div>
-      <div></div>
-    </li>
+    <form className={classes.form}>
+      <Input
+        label='Amount'
+        input={{
+          id: 'amount',
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
+      />
+      <button>+ Add</button>
+    </form>
   );
 };
 
-export default MealItem;
+export default MealItemForm;
